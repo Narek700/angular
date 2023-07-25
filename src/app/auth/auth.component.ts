0a6041f23704fba480eaa8c8c353c7a2c583dc7e
Narek.Mirzoyan <narek.mirzoyan@idt.am>
2023-07-25 18:37:45 +0400

    add auth component 25/07/2023

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+    isLoginMode = true;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(form: NgForm) {
+        console.log(form.value);
+        form.reset();
+    }
 }
